4be4e9d45e082fcdea41dcba62ebc2cb4260b6b5
Emily Adams <emily.adams.nh@gmail.com>
2026-06-08 21:40:12 -0400

    Built backend/app/models files

--- app/backend/models/portfolio.py
+++ app/backend/models/portfolio.py
@@ -1,1 +1,28 @@
 # Table file for portfolios, assets, transactions, and other factors
+
+from sqlalcehmy import Column, Integer, String, DateTime 
+from sqlalchemy.orm import relationship 
+from datetime import datetime 
+from app.database import Base 
+
+class Portfolio(Base) : 
+    __tablename__ = "portfolios"
+
+    id = Column( Integer, primary_key=True, index=True )
+    name = Column( String, nullable=False ) 
+    owner_id = Column( Integer, ForeignKey( "user.id" ), nullable=False )
+    created_at = Column( DateTime, default=datetime.utcnow )
+
+    owner = relationship( "User", back_populates="portfolios" )
+    assets = relationship( "Assets", back_populates="portfolio" )
+
+class Asset(Base) : 
+    __tablename__ = "assets"
+
+    id = Column( Integer, primary_key=True, index=True )
+    ticker = Column( String, nullable=False )
+    shares = Column( Float, nullable=False )
+    purchase_price = Column( Integer, ForeignKey( "portfolio.id" ), nullable=False )
+    created_at = Column( DateTime, default=datetime.utcnow )
+
+    portfolio = relationship( "Portfolio", back_populates="assets" )
--- app/backend/models/user.py
+++ app/backend/models/user.py
@@ -1,1 +1,17 @@
 # For user data table file
+
+from sqlalcehmy import Column, Integer, String, DateTime 
+from sqlalchemy.orm import relationship 
+from datetime import datetime 
+from app.database import Base 
+
+class User(Base) : 
+    __tablename__ = "users"
+
+    id = Column( Integer, primary_key=True, index=True )
+    email = Column( String, unique=True, index=True, nullable=False )
+    hashed_password = Column( String, nullable=False )
+    full_name = Column( String, nullable=True )
+    created_at = Column( DateTime, default=datetime.utcnow )
+
+    portfolios = relationship( "Portfolio", back_populates="owner" )
